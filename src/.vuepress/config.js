a85f39b0c3b5d13a2e101a436b43f8795363b309
Dan Jones <dan@lunarfish.co.uk>
2024-02-12 20:59:41 +0000

    fix: update og meta tags

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -10,10 +10,12 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['meta', { name: "og:title", content:"Wealth Scales by lunarfish"}],
-    ['meta', { name: "og:description", content:description}],
-    ['meta', { name: "og:image", content:"//lunarfish.github.io/wealthscales/assets/images/wealthscales_preview.png"}],
-    ['meta', { name: "og:url", content: "//lunarfish.github.io/wealthscales/"}]
+    ['meta', { property: "og:title", content:"Wealth Scales by lunarfish"}],
+    ['meta', { property: "og:type", content:"article"}],
+    ['meta', { property: "og:description", content:description}],
+    ['meta', { property: "og:image", content:"//lunarfish.github.io/wealthscales/assets/images/wealthscales_preview.png"}],
+    ['meta', { property: "og:url", content: "//lunarfish.github.io/wealthscales/"}],
+    ['meta', { name: "twitter:card", content: "summary_large_image"}]
   ],
   themeConfig: {
     repo: '',
